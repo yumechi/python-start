0f339b5f55afbc6ad2413cd6b4cf6c5fc66a9379
yumechi <yumechi0525@gmail.com>
2020-01-30 20:25:26 +0900

    一旦完成

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ def get_args():
     parser.add_argument('-d', '--data', type=str, required=True, help='json data')
     parser.add_argument('-t', '--template', type=str, required=True, help='template data')
     # count とかも足してみます
-
+    parser.add_argument('-c', '--count', type=int, help='template data', default=1)
+ 
     return parser.parse_args()
 
 def create_message(args):
@@ -18,10 +19,21 @@ def create_message(args):
     data = args.data
 
     # ここをやっていきましょう
+
     message = "デフォルトメッセージ"
 
-    return message
+    with open(template, "r") as f:
+        template_data = f.read()
+
+    with open(data, "r") as f:
+        json_data = json.load(f)
+
+    template_data =  template_data.format(**json_data)
+
+    return template_data
 
 if __name__ == "__main__":
     args = get_args()
-    print(create_message(args))
+    count = args.count
+    for i in range(count):
+        print(create_message(args))
